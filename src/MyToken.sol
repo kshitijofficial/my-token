971f13b399fd5453f2dc5af6f9a9f67e775d7b7f
Kshitij <srivastavakshitijprofessional@gmail.com>
2026-06-20 05:37:15 +0000

    initial

---
+++ src/MyToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.34;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20{
+   constructor()ERC20("demo","dm"){
+      
+   }
+  
+}
---
+++ test/MyToken.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {MyToken} from "../src/MyToken.sol";
+
+contract MyTokenTest is Test {
+    MyToken public token;
+
+    function setUp() public {
+        token = new MyToken();
+    }
+}
